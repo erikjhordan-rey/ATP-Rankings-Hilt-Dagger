9a51275ebe7108d5f4e35896530c2a0edac62fa4
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-09-30 23:55:37 -0500

    Update libs

--- app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/domain/GetPlayersUseCase.kt
+++ app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/domain/GetPlayersUseCase.kt
@@ -1,11 +1,9 @@
 package io.github.erikjhordanrey.atp_rankings_di.domain
 
-import androidx.annotation.WorkerThread
 import io.github.erikjhordanrey.atp_rankings_di.data.PlayerRepository
 import javax.inject.Inject
 
 class GetPlayersUseCase @Inject constructor(private val playerRepository: PlayerRepository) {
 
-    @WorkerThread
     suspend fun getAllPlayers() = playerRepository.getAllPlayers()
 }
