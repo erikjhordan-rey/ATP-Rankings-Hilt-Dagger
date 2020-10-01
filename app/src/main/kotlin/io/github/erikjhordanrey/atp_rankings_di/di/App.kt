534dd442c61712aaea8a97d7197403f9de6b247c
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-09-30 20:08:03 -0500

    Delete unused code

--- app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/di/App.kt
+++ app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/di/App.kt
@@ -3,18 +3,8 @@ package io.github.erikjhordanrey.atp_rankings_di.di
 import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.content.res.Resources
-import android.util.TypedValue
-
-
-
 
 class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-    }
 
     private val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder().application(this).build()
@@ -28,10 +18,3 @@ class App : Application() {
 
 }
 fun Activity.appComponent() = App.appComponent(this)
-
-fun getColorByThemeAttr(context: Context, attr: Int, defaultColor: Int): Int {
-    val typedValue = TypedValue()
-    val theme: Resources.Theme = context.theme
-    val got: Boolean = theme.resolveAttribute(attr, typedValue, true)
-    return if (got) typedValue.data else defaultColor
-}
