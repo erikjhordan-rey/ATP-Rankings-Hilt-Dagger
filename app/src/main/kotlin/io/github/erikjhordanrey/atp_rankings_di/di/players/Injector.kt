043ff1eae28579f950106c5bf25d24a3a4a8b018
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-09-30 20:22:24 -0500

    Add Single ViewModel Factory

--- app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/di/players/Injector.kt
+++ app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/di/players/Injector.kt
@@ -5,9 +5,7 @@ import io.github.erikjhordanrey.atp_rankings_di.ui.PlayersActivity
 
 fun inject(playersActivity: PlayersActivity) = playersActivity.run {
     DaggerPlayersComponent.builder()
-
-       // .playersUiModule(PlayersUiModule(this))
-        .appComponent(appComponent())
-        .build()
-        .inject(this)
+            .appComponent(appComponent())
+            .build()
+            .inject(this)
 }
--- app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/ui/PlayersActivity.kt
+++ app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/ui/PlayersActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.erikjhordanrey.atp_rankings_di.core.extension.compositeScaleTransform
 import io.github.erikjhordanrey.atp_rankings_di.core.extension.liveDataObserve
+import io.github.erikjhordanrey.atp_rankings_di.core.extension.viewModelProvidersOf
 import io.github.erikjhordanrey.atp_rankings_di.databinding.ActivityPlayersBinding
+import io.github.erikjhordanrey.atp_rankings_di.di.players.PlayersViewModelFactory
 import io.github.erikjhordanrey.atp_rankings_di.di.players.inject
 import io.github.erikjhordanrey.atp_rankings_di.domain.Player
 import javax.inject.Inject
 
 class PlayersActivity : AppCompatActivity() {
 
-    // @Inject lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var binding: ActivityPlayersBinding
 
     private val playersAdapter by lazy { PlayersAdapter() }
 
-    private lateinit var binding: ActivityPlayersBinding
-
     @Inject
+    lateinit var playersViewModelFactory: PlayersViewModelFactory
+
     lateinit var viewModel: PlayersViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +27,10 @@ class PlayersActivity : AppCompatActivity() {
         binding = ActivityPlayersBinding.inflate(layoutInflater)
         setContentView(binding.root)
         inject(this)
-        //viewModel = viewModelProvidersOf(viewModelFactory)
+        viewModel = viewModelProvidersOf(playersViewModelFactory)
         initObservers()
         initUi()
         viewModel.loadPlayers()
-
     }
 
     private fun initUi() = binding.viewPager.run {
