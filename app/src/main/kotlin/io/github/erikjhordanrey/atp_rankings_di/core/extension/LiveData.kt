de96e4528f35e26cc0898b2d5e2560d34a650076
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-10-08 16:54:45 -0500

    Remove LiveData extension

--- app/src/main/kotlin/io/github/erikjhordanrey/atp_rankings_di/core/extension/LiveData.kt
+++
@@ -1,8 +0,0 @@
-package io.github.erikjhordanrey.atp_rankings_di.core.extension
-
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-
-fun <T : Any, L : LiveData<T>> LifecycleOwner.liveDataObserve(liveData: L, body: (T?) -> Unit) =
-        liveData.observe(this, Observer(body))
